df7fc92661241f9a49fef41a2037ba4e7222842c
Anael <166033786+Anael-Fer@users.noreply.github.com>
2024-11-11 16:10:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+
+    static bool MatrizIdentidade(int[,] matriz)
+    {
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                // Verifica se a diagonal principal é 1 e os outros elementos são 0
+                if ((i == j && matriz[i, j] != 1) || (i != j && matriz[i, j] != 0))
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+    static void Main()
+    {
+        int[,] matriz = {
+            { 1, 0, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 0, 1, 0 },
+            { 0, 0, 0, 1 }
+        };
+
+        bool resultado = MatrizIdentidade(matriz);
+        Console.WriteLine("A matriz é identidade? " + resultado);
+    }
+
+
+}
